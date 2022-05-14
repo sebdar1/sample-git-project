7329bcbcad6eacf856d53a6777c1dd5927c60aaf
sebdar901 <sebdar901@gmail.com>
2022-05-14 21:07:13 +0200

    Dodanie klasy Calculator z metodami i klasy Department

--- src/main/java/org/gitsample/Company.java
+++ src/main/java/org/gitsample/Company.java
@@ -1,13 +1,10 @@
 package org.gitsample;
 
 public class Company {
-	
 	private String name;
 	private String code;
-	
 	@Override
 	public String toString() {
 		return " [name=" + name + ", code=" + code + "]";
 	}
-	
 }
