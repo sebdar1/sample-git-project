66a4d591bedb2f1c8a2148e0cc22e509aaa4a649
sebdar1 <105283515+sebdar1@users.noreply.github.com>
2022-05-17 18:15:09 +0200

    Test update 1

--- src/main/java/org/gitsample/Main.java
+++ src/main/java/org/gitsample/Main.java
@@ -1,9 +1,10 @@
 package org.gitsample;
-
+// Sample change 123
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Test Main !! ");
+		System.out.println("Hi this is a change !! ");
 	}
 
 }
